8068adb4a6fb5b7f951160b3b6721a148fa638d0
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-26 04:27:55 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -8,10 +8,11 @@ class Person
   def get_married(person)
     self.partner = person
     if person.class != Person
-      raise PartnerError
-    rescue => error
-      puts error.message
-    end
+      begin
+        raise PartnerError
+      rescue => error
+        puts error.message
+      end
     else
       person.partner = self
     end
